19fb8423f03994577ce3641041b88dc319abac28
taro <lautaro@airtm.io>
2022-10-01 20:27:27 -0300

    refactor, manage users leaving, send current user list to just-joined user

--- src/index.js
+++ src/index.js
@@ -15,20 +15,50 @@ const port = process.env.PORT || 8080
 
 const webSocketServer = new WebSocketServer({ port })
 
+const clientToUser = new Map()
+
 webSocketServer.on('connection', function (client) {
   console.log('WebSocket Client Connected')
+
+  const sendToEveryoneElse = (message, isBinary = false) => {
+    console.log('sendToEveryoneElse', message.toString())
+    webSocketServer.clients.forEach(secondClient => {
+      const userId = clientToUser.get(secondClient)
+
+      console.log('Trying to send to client', message.toString(), userId, secondClient === client, secondClient.readyState)
+      if (client === secondClient)
+        return
+      if (secondClient.readyState === WebSocket.OPEN)
+        return
+      secondClient.send(message, { binary: isBinary })
+    })
+  }
 
   client.on('message', (message, isBinary) => {
     console.log('WebSocket Server Received message.', message.toString())
 
-    webSocketServer.clients.forEach(secondClient => {
-      if (client === secondClient)
-        return
-      console.log('sending to client', secondClient.readyState, WebSocket.OPEN)
-      if (secondClient.readyState === WebSocket.OPEN) {
-        secondClient.send(message, { binary: isBinary })
-      }
-    })
+    const parsedMessage = JSON.parse(message)
+
+    if (parsedMessage.action === 'join') {
+      clientToUser.set(client, parsedMessage.userId)
+    }
+
+    sendToEveryoneElse(message, isBinary)
+    client.send(JSON.stringify({
+      action: 'userList',
+      users: [...webSocketServer.clients].map(client => clientToUser.get(client))
+    }))
+  })
+
+  client.on('close', () => {
+    console.log('WebSocket Client Closed')
+
+    sendToEveryoneElse(JSON.stringify({
+      action: 'leave',
+      userId: clientToUser.get(client),
+    }))
+
+    clientToUser.delete(client)
   })
 
 })
